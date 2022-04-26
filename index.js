81c46cb0de402d24d449a3ebd859d6f5b6bec300
Lorelain <baldomerland@gmail.com>
2022-04-26 15:14:41 +0200

    edad1

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Tu código va aquí: 
+let edad=16;
+if (edad < 14){
+    console.log(Lo siento, eres demasiado joven, vuelve a intentarlo en unos años.)
+}
+else if (){
+    console.log(
+}
+
+Puedes entrar, pero sólo si te acompaña una persona mayor de edad.
+else if (){
+    console.log(
+}
+
+...
+else
+
+
+   
+   ≥
+   Eres mayor de edad, puedes entrar.
