fa34be5feb294ccc7bd86c3dc89b534bde7010aa
Lorelain <baldomerland@gmail.com>
2022-04-27 01:34:45 +0200

    Años

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-// Tu código va aquí: 
 
-function Ticket(años){
+let edad=16
 
-    if (años < 14){
-        console.log("Lo siento, eres demasiado joven, vuelve a intentarlo en unos años.")
-    }
+if(edad < 14) {
+    console.log ("Lo siento, eres demasiado joven, vuelve a intentarlo en unos años.")
 
-    else if (años >= 14 && <= 17){
-        console.log("Puedes entrar, pero con un acompañante +18.")
+} if else(edad >= 14 && edad <= 17) {
+    console.log("Puedes entrar, pero sólo si te acompaña una persona mayor de edad.")
+
+} if else(edad > 17) {
+    console.log ("Eres mayor de edad, puedes entrar.")
+}
+/*-----*/
+
+function Ticket (años) {
+    if(años < 14) {
+        console.log ("Lo siento, eres demasiado joven, vuelve a intentarlo en unos años.")
+    } if (años >= 14 && años <= 17) {
+        console.log("Puedes entrar, pero sólo si te acompaña una persona mayor de edad.")
+    } if (años > 17) {
+        console.log ("Adelante y disfruta.")
     }
-        
-    else (años =<18){
-        console.log("Adelante, y disfruta.")
-    }
-    
 }
 
 
